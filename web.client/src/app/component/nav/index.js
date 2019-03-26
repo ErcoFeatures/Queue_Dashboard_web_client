f794133b924c880bac8748b84a8c9d69b90336d3
Patrice Diouf <pdiouf@cebglobal.com>
2019-03-26 15:50:22 -0400

    refactor

--- web.client/src/app/component/nav/index.js
+++ web.client/src/app/component/nav/index.js
@@ -13,10 +13,10 @@ const QDNav = (props) => {
         <div id="content">
         <div className="main-container">
             <div className="col-xs-12 main-grid no-padding">
-                <div className="col-xs-2 menu-part no-padding">
+                <div className="col-xs-1 menu-part no-padding">
                     <LeftMenu {...props}/>
                 </div>
-                <div className="col-xs-10 no-padding">
+                <div className="col-xs-11 no-padding">
                     <div className="header-part">
                         <TopHeader {...props}/>
                     </div>
--- web.client/src/app/translations/fr.js
+++ web.client/src/app/translations/fr.js
@@ -1,7 +1,7 @@
 export default {
     'app.name': 'Queue Dashboard',
     'QD.home.title': 'Queue Dashboard',
-    'components.header.buttons.login': 'Se connecter avec Facebook',
+    'components.header.buttons.login': 'Se connecter',
     'components.header.buttons.logout': 'se deconnecter',
     'components.header.buttons.signup': "S'enregistrer",
 
